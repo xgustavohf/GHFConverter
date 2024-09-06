5fa0a508b58ee9f42734bfa8e0e0fb23067b45d0
Gustavo Ferreira <gustavohferreira@live.com>
2024-09-06 20:41:10 -0300

    Update settings.py

--- ghfconverter/settings.py
+++ ghfconverter/settings.py
@@ -144,3 +144,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+'''
